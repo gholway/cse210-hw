bc978bed4d870d2dda487d7dfb65077f99d452ae
gholway <131705574+gholway@users.noreply.github.com>
2023-10-03 17:15:04 -0600

    W03fix

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,39 +1,57 @@
 using System;
 using System.Collections.Generic;
-
-private class scripture
+class Program
 {
-    private void passage(string sPas, string sRef)
+    static void Main(string[] args)
     {
-        List<string> scripturePassage = new List<string>(sPas.parse);
+        Console.WriteLine("Enter a Scripture to memorize.");
+        string sRef = Console.ReadLine();
+        Console.WriteLine("Enter passage text.");
+        string sPas = Console.ReadLine();
+        scripture script = new scripture();
+        script.passage(sPas, sRef);
+        word hide = new word();
+        hide.hider(sPas, sRef);
+    }
+}
+public class scripture
+{
+    public void passage(string sPas, string sRef)
+    {
+        List<string> scripturePassage = new List<string>(sPas.Split(' '));
         Console.WriteLine($"Your Scripture to memorize is: {sRef}\n{sPas}");
     }
 }
-private class word
+public class word
 {
-    private void hider(string sPas, string sRef)
+    public void hider(string sPas, string sRef)
     {
         Console.WriteLine("Press enter for memorization challange.\nEnter quit to exit.\nEnter reference to see unhidden passage and reference.");
         string action = Console.ReadLine();
         bool repeat = false;
         int hidden = 0;
-        List<string> hidingPassage = new List<string>(sPas.parse);
-        int toHide = hidingPassage.count;
+        List<string> hidingPassage = new List<string>(sPas.Split(' '));
+        int toHide = 0;
+        foreach(string passage in hidingPassage)
+        {
+            toHide += 1;
+        }
         while(hidden < toHide)
         {
             Console.Clear();
-            Random wordPicker = new Random(0, toHide);
-            int picked = wordPicker;
-            string selection = hidingPassage.index(picked);
-            if(action.lower == "quit")
+            Random wordPicker = new Random(toHide);
+            int picked = wordPicker.Next();
+            string selection = hidingPassage[picked];
+            if(action.ToLower() == "quit")
             {
                 toHide = 0;
             }
-            else if(action.lower == "reference")
+            else if(action.ToLower() == "reference")
             {
-                reference.scriptureReferance(sPas, sRef);
+                reference remind = new reference();
+                remind.scriptureReferance(sPas, sRef);
                 Console.WriteLine("Press enter to continue memorization or quit to exit.");
-                string action = Console.ReadLine();
+                action = Console.ReadLine();
             }
             else
             {
@@ -43,32 +61,20 @@ private class word
                 }
                 if(repeat == false)
                 {
-                    hidingPassage.index(picked) = "__";
+                    hidingPassage[picked] = "__";
                 }
                 repeat = false;
                 Console.WriteLine("Press enter to continue.\nEnter quit to exit.\nEnter reference to see unhidden passage and reference.");
 
             }
         }
-        this.Close();
+        System.Environment.Exit(0);
     }
 }
-private class reference
+public class reference
 {
-    private void scriptureReferance(string sPas, string sRef)
+    public void scriptureReferance(string sPas, string sRef)
     {
         Console.WriteLine($"The Scripture book, chapter, and verse(s) are {sRef}\nWhich reads as follows:\n{sPas}");
-    }
-}
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Enter a Scripture to memorize.");
-        string sRef = Console.ReadLine();
-        Console.WriteLine("Enter passage text.");
-        string sPas = Console.ReadLine();
-        scripture.passage(sPas, sRef);
-        word.hider(sPas, sRef);
     }
 }
