3fd129ad6e06ec3e7ef6b38e1309a31a3d689ce9
gholway <131705574+gholway@users.noreply.github.com>
2023-10-23 16:23:25 -0600

    W07prove

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,231 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.IO;
+using System.Data.SqlTypes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        List<string> events = new List<string>();
+        string plan = "";
+        while(plan != "finish")
+        {
+            Console.WriteLine("Enter 'lecture', 'reception', or 'outdoor' to plan an event or enter 'finish' to exit.\nIf you wish to see the planned events enter 'display'");
+            plan = Console.ReadLine();
+            if(plan == "lecture")
+            {
+                Lecture LPlan = new Lecture();
+                Console.WriteLine("Do you want to make a 'normal', 'long', or 'short' discription?");
+                plan = Console.ReadLine();
+                if(plan == "normal")
+                {
+                    string addition = LPlan.Details();
+                    events.Add(addition);
+                }
+                else if(plan == "long")
+                {
+                    string addition = LPlan.MDetails();
+                    events.Add(addition);
+                }
+                else if(plan == "short")
+                {
+                    string addition = LPlan.Description();
+                    events.Add(addition);
+                }
+            }
+            else if(plan == "reception")
+            {
+                Reception RPlan = new Reception();
+                Console.WriteLine("Do you want to make a 'normal', 'long', or 'short' discription?");
+                plan = Console.ReadLine();
+                if(plan == "normal")
+                {
+                    string addition = RPlan.Details();
+                    events.Add(addition);
+                }
+                else if(plan == "long")
+                {
+                    string addition = RPlan.MDetails();
+                    events.Add(addition);
+                }
+                else if(plan == "short")
+                {
+                    string addition = RPlan.Description();
+                    events.Add(addition);
+                }
+            }
+            else if(plan == "outdoor")
+            {
+                OutdoorGathering OPlan = new OutdoorGathering();
+                Console.WriteLine("Do you want to make a 'normal', 'long', or 'short' discription?");
+                plan = Console.ReadLine();
+                if(plan == "normal")
+                {
+                    string addition = OPlan.Details();
+                    events.Add(addition);
+                }
+                else if(plan == "long")
+                {
+                    string addition = OPlan.MDetails();
+                    events.Add(addition);
+                }
+                else if(plan == "short")
+                {
+                    string addition = OPlan.Description();
+                    events.Add(addition);
+                }
+            }
+            else if(plan == "display")
+            {
+                Console.WriteLine(events);
+            }
+        }
+    }
+}
+public abstract class Event
+{
+    public abstract string Details();
+    public abstract string MDetails();
+    public abstract string Description();
+}
+public class Lecture: Event
+{
+    public override string Details()
+    {
+        Console.WriteLine("Enter the title of the event.");
+        string title = Console.ReadLine();
+        Console.WriteLine("Enter the day and time the event will take place.");
+        string time = Console.ReadLine();
+        Address sAddress = new Address();
+        string place = sAddress.SAddress();
+        Console.WriteLine("Enter discription.");
+        string discription = Console.ReadLine();
+        string format = "title: " + title + "\n" + discription + "\nWill be on " + time + "\nAt: " + place;
+        return format;
+    }
+    public override string MDetails()
+    {
+        string type = "lecture";
+        Console.WriteLine("Enter the title of the event.");
+        string title = Console.ReadLine();
+        Console.WriteLine("Enter the day and time the event will take place.");
+        string time = Console.ReadLine();
+        Address sAddress = new Address();
+        string place = sAddress.SAddress();
+        Console.WriteLine("Enter short discription.");
+        string discription = Console.ReadLine();
+        Console.WriteLine("Enter speaker name.");
+        string speaker = Console.ReadLine();
+        Console.WriteLine("Enter how many people can attend.");
+        string capasity = Console.ReadLine();
+        string format = "title:" + title + "\nEvent type: " + type + "\nBy: " + speaker + "\n" + discription + "\nWill be on " + time + "\nAt: " + place + "\nThere are " + capasity + " seats avaible.";
+        return format;
+    }
+    public override string Description()
+    {
+        string type = "lecture";
+        Console.WriteLine("Enter the title of the event.");
+        string title = Console.ReadLine();
+        Console.WriteLine("Enter the day this will take place.");
+        string day = Console.ReadLine();
+        string format = "Event type: " + type + "\ntitle: " + title + "\nOn: " + day;
+        return format;
+    }
+}
+public class Reception: Event
+{
+    public override string Details()
+    {
+        Console.WriteLine("Enter the title of the event.");
+        string title = Console.ReadLine();
+        Console.WriteLine("Enter the day and time the event will take place.");
+        string time = Console.ReadLine();
+        Address sAddress = new Address();
+        string place = sAddress.SAddress();
+        Console.WriteLine("Enter short discription.");
+        string discription = Console.ReadLine();
+        string format = "title:" + title + "\n" + discription + "\nWill be on " + time + "\nAt: " + place;
+        return format;
+    }
+    public override string MDetails()
+    {
+        string type = "reception";
+        Console.WriteLine("Enter the title of the event.");
+        string title = Console.ReadLine();
+        Console.WriteLine("Enter the day and time the event will take place.");
+        string time = Console.ReadLine();
+        Address sAddress = new Address();
+        string place = sAddress.SAddress();
+        Console.WriteLine("Enter short discription.");
+        string discription = Console.ReadLine();
+        Console.WriteLine("Enter how many people can attend.");
+        string capasity = Console.ReadLine();
+        Console.WriteLine("Enter email for RSVP.");
+        string email = Console.ReadLine();
+        string format = "title:" + title + "\n" + type + "\n" + discription + "\nWill be on " + time + "\nAt: " + place + "\nThere are " + capasity + " seats avaible visit " + email + " to enter your reservation.";
+        return format;
+    }
+    public override string Description()
+    {
+        string type = "reception";
+        Console.WriteLine("Enter the title of the event.");
+        string title = Console.ReadLine();
+        Console.WriteLine("Enter the day this will take place.");
+        string day = Console.ReadLine();
+        string format = "Event type: " + type + "\ntitle: " + title + "\nOn: " + day;
+        return format;
+    }
+}
+public class OutdoorGathering: Event
+{
+    public override string Details()
+    {
+        Console.WriteLine("Enter the title of the event.");
+        string title = Console.ReadLine();
+        Console.WriteLine("Enter the day and time the event will take place.");
+        string time = Console.ReadLine();
+        Address sAddress = new Address();
+        string place = sAddress.SAddress();
+        Console.WriteLine("Enter short discription.");
+        string discription = Console.ReadLine();
+        string format = "title:" + title + "\n" + discription + "\nWill be on " + time + "\nAt: " + place;
+        return format;
+    }
+    public override string MDetails()
+    {
+        string type = "outdoor gathering";
+        Console.WriteLine("Enter the title of the event.");
+        string title = Console.ReadLine();
+        Console.WriteLine("Enter the day and time the event will take place.");
+        string time = Console.ReadLine();
+        Address sAddress = new Address();
+        string place = sAddress.SAddress();
+        Console.WriteLine("Enter short discription.");
+        string discription = Console.ReadLine();
+        Console.WriteLine("Enter expected weather during the event.");
+        string weather = Console.ReadLine();
+        string format = "title:" + title + "\n" + type + "\n" + discription + "\nWill be on " + time + "\nAt: " + place + "\nIt expected to be " + weather + " during this event.";
+        return format;
+    }
+    public override string Description()
+    {
+        string type = "outdoor gathering";
+        Console.WriteLine("Enter the title of the event.");
+        string title = Console.ReadLine();
+        Console.WriteLine("Enter the day this will take place.");
+        string day = Console.ReadLine();
+        string format = "Event type: " + type + "\ntitle: " + title + "\nOn: " + day;
+        return format;
+    }
+}
+public class Address
+{
+    public string SAddress()
+    {
+        Console.WriteLine("Enter address of event.");
+        string sAddress = Console.ReadLine();
+        return sAddress;
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,91 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.IO;
+using System.Data.SqlTypes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<string> facilityUse = new List<string>();
+        string track = "";
+        while(track != "exit")
+        {
+            Console.WriteLine("Enter 'run', 'bike', or 'swim' to track an activity.\nEnter 'display' to see entries or 'exit' to finish.");
+            track = Console.ReadLine();
+            if(track == "run")
+            {
+                Running run = new Running();
+                facilityUse.Add(run.format());
+            }
+            else if(track == "bike")
+            {
+                Biking bike = new Biking();
+                facilityUse.Add(bike.format());
+            }
+            else if(track == "swim")
+            {
+                Swimming swim = new Swimming();
+                facilityUse.Add(swim.format());
+            }
+            else if(track == "display")
+            {
+                Console.WriteLine(facilityUse);
+            }
+        }
+    }
+}
+public abstract class Activity
+{
+    public abstract string format();
+}
+public class Running: Activity
+{
+    public override string format()
+    {
+        Console.WriteLine("Enter distance ran in km.");
+        float distance = float.Parse(Console.ReadLine());
+        Console.WriteLine("Enter time in minutes spent running.");
+        float time = float.Parse(Console.ReadLine());
+        float speed = distance / time * 60;
+        float pace = time / distance;
+        Console.WriteLine("Enter day of run as: day/month/year");
+        string day = Console.ReadLine();
+        string format = day + " Running (" + time + " min): Distance " + distance + "kmh, Speed " + speed + ", pace " + pace + " min per km.";
+        return format;
+    }
+}
+public class Biking: Activity
+{
+    public override string format()
+    {
+        Console.WriteLine("Enter distance biked in km.");
+        float distance = float.Parse(Console.ReadLine());
+        Console.WriteLine("Enter time in minutes spent biking.");
+        float time = float.Parse(Console.ReadLine());
+        float speed = distance / time * 60;
+        float pace = time / distance;
+        Console.WriteLine("Enter day of bike run as: day/month/year");
+        string day = Console.ReadLine();
+        string format = day + " Biking (" + time + " min): Distance " + distance + "kmh, Speed " + speed + ", pace " + pace + " min per km.";
+        return format;
+    }
+}
+public class Swimming: Activity
+{
+    public override string format()
+    {
+        Console.WriteLine("Enter laps in pool.");
+        float laps = float.Parse(Console.ReadLine());
+        float distance = laps * 50;
+        Console.WriteLine("Enter time in minutes spent swimming.");
+        float time = float.Parse(Console.ReadLine());
+        float speed = distance / time * 60;
+        float pace = time / distance;
+        Console.WriteLine("Enter day of swim as: day/month/year");
+        string day = Console.ReadLine();
+        string format = day + " Swimming (" + time + " min): Laps " + laps + ", Distance " + distance + "kmh, Speed " + speed + ", pace " + pace + " min per km.";
+        return format;
     }
 }
